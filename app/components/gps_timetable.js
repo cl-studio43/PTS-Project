aa74a636c3f65575f07e7197093c89290617638f
Lyon <lyon.huang@prophetdigits.com>
2019-08-06 20:58:48 +0800

    update data

--- app/components/gps_timetable.js
+++ app/components/gps_timetable.js
@@ -3,9 +3,8 @@ import { Button } from 'antd'
 import newsUp from '../images/newsUp.png'
 import PBright from '../images/PBright.png'
 import "./gps_timetable.css"
-import gpsMR from '../images/gpsMR.png'
-import gpsML from '../images/gpsML.png'
-
+import gpsR from '../images/gps_R.png'
+import gpsL from '../images/gps_L.png'
 
 
 export default class GPS extends Component {
@@ -16,8 +15,8 @@ export default class GPS extends Component {
                     <div className="section-content ">
                         <div className="gpsphone">
                             <div >
-                                <div style={{width: "90%", margin:"0 auto", cursor: "pointer"}}  onClick={() => window.open("http://med.tori.narlabs.org.tw/Ship/gpsOutput/showGPStMap.php")}>
-                                    <img src={gpsML} alt="開啟GPS" />
+                                <div style={{width: "90%", margin:"0 auto"}} >
+                                    <img src={gpsL} alt="map" />
                                 </div>
                                 <div className="timeTableBlock">
                                     <div className="timeTable">
@@ -33,9 +32,8 @@ export default class GPS extends Component {
                                         <div className="time">
                                             <div
                                                 style={{ width: "50%"}}
-                                                onClick={() => window.open("https://docs.google.com/spreadsheets/d/1sAEsifwTcuck1DE8JCdlRXFb-VtsWTjvp6v28Mh6BXQ/edit?usp=sharing")}
                                             >
-                                                <img src={gpsMR} alt="開啟GPS" />
+                                                <img src={gpsR} alt="wave" />
                                         </div>
                                             <img style={{ width: "45%" }} src={PBright} alt="wave" />
                                         </div>
@@ -46,15 +44,6 @@ export default class GPS extends Component {
                         <div className="gpsweb">
                             <div className="GPSnTime">
                                 <div className="GPSBlock">
-                                    <div onClick={() => window.open("http://med.tori.narlabs.org.tw/Ship/gpsOutput/showGPStMap.php")}>
-                                        <div className="gpsTitle">
-                                            <span>想知道航行進度 ?</span>
-                                            <span>點我看船隻位置 !</span>
-                                        </div>
-                                        <div className="gpsLocation">
-                                            開啟GPS定位
-                                    </div>
-                                    </div>
                                 </div>
                                 <div className="timeTableBlock">
                                     <div className="timeTable">
@@ -68,56 +57,13 @@ export default class GPS extends Component {
                                             </div>
                                         </div>
                                         <div className="time">
-                                            <div
-                                                className="programButton"
-                                                onClick={() => window.open("https://docs.google.com/spreadsheets/d/1sAEsifwTcuck1DE8JCdlRXFb-VtsWTjvp6v28Mh6BXQ/edit?usp=sharing")}
-                                            >
-                                                節目表
-                                        </div>
+                                            <img className="programButton" src={gpsR} alt="wave" />
                                             <img style={{ width: "45%" }} src={PBright} alt="wave" />
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
-
-
-
-                        {/* <div className="GPSnTime">
-                            <div className="GPSBlock">
-                                <div onClick={() => window.open("http://med.tori.narlabs.org.tw/Ship/gpsOutput/showGPStMap.php")}>
-                                    <div className="gpsTitle">
-                                        <span>想知道航行進度 ?</span>
-                                        <span>點我看船隻位置 !</span>
-                                    </div>
-                                    <div className="gpsLocation">
-                                        開啟GPS定位
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="timeTableBlock">
-                                <div className="timeTable">
-                                    <div>
-                                        <img src={newsUp} alt="chart" />
-                                    </div>
-                                    <div className="programnews">
-                                        <div style={{ margin: "35px" }}>
-                                            <h5 style={{ marginBottom: "unset", color: "#FFF" }}>{"即時訊息 : "}</h5>
-                                            <h6 style={{ marginBottom: "unset", color: "#FFF" }}>{this.props.programNews}</h6>
-                                        </div>
-                                    </div>
-                                    <div className="time">
-                                        <div
-                                            className="programButton"
-                                            onClick={() => window.open("https://docs.google.com/spreadsheets/d/1sAEsifwTcuck1DE8JCdlRXFb-VtsWTjvp6v28Mh6BXQ/edit?usp=sharing")}
-                                        >
-                                            節目表
-                                        </div>
-                                        <img style={{ width: "45%" }} src={PBright} alt="wave" />
-                                    </div>
-                                </div>
-                            </div>
-                        </div> */}
                     </div>
                 </section>
             </div>
